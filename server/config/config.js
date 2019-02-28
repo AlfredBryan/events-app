d94cdbf6af8f0b8e2925759a9390a9e2c23d19eb
AlfredBryan <alfred.chimereze@gmail.com>
2019-02-28 11:23:20 +0100

    Integrate heroku database

---
+++ server/config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    databaseUrl: process.env.DATABASE_URL,
+    dialect: "postgres",
+    freezeTableName: true
+  },
+  test: {
+    databaseUrl: process.env.DATABASE_URL,
+    dialect: "postgres"
+  },
+  production: {
+    databaseUrl: process.env.DATABASE_URL,
+    dialect: "postgres"
+  }
+};
